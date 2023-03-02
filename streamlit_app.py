a64b7641b8c2fec92a7194052bb25a932853213c
targabor <65847967+targabor@users.noreply.github.com>
2023-03-02 17:14:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,6 @@ try:
 except URLError as e:
   streamlit.error()
 
-streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from FRUIT_LOAD_LIST")
